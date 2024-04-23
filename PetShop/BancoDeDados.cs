520420de6c2b8ea180a900daf28439ca94611aff
GabrielDittrich <gdittrchcj@gmail.com>
2024-04-23 10:14:45 -0300

    MySql + Swagger

--- PetShop/BancoDeDados.cs
+++ PetShop/BancoDeDados.cs
@@ -5,9 +5,16 @@ public class BancoDeDados : DbContext
     //Configuração da conexão
     protected override void OnConfiguring(DbContextOptionsBuilder builder)
     {
-        builder.UseMySQL("server=localhost;port=3306;database=restaurante;user=root;password=positivo");
+        builder.UseMySQL("server=localhost;port=3306;database=petshop;user=root;password=positivo");
     }
 
     //Mapeamento das tabelas
     public DbSet<Animal> Animais { get; set; }
+    public DbSet<Produto> Produtos { get; set; }
+    public DbSet<Pessoa> Pessoas { get; set; }
+
+
+    //Comandos dotnet
+    //dotnet ef migrations add CriarTabelaProduto
+    //dotnet ef database update
 }
