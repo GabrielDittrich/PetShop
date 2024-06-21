9ee276f951cb006101ec2a502d476ae2687b8e72
Gabriel Dittrich <gdittrchcj@gmail.com>
2024-06-21 17:06:48 -0300

    Ultima atualização

--- app-front/src/paginas/Home.jsx
+++ app-front/src/paginas/Home.jsx
@@ -1,8 +1,11 @@
+import React from 'react';
+import MinhaImagem from '../imagens/golden-retriever.jpg'; // Importe a imagem
 
 function Home() {
     return (
-        <div class="principal">
-            <h3>PetShop</h3>
+        <div className="principal">
+            <h3>Seja Bem-Vindo Ao PetShop</h3>
+            <img src={MinhaImagem} alt="Imagem de Boas-Vindas" /> {/* Use a variável que armazena o caminho da imagem */}
         </div>
     );
 }
